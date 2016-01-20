c1f300f9e4e46abf4da381bb6ebfed1ef8b6a73f
Will Lopez <will.lopez77@gmail.com>
2016-01-19 21:20:01 -0800

    Some clean up

--- client/actions/main-view.js
+++ client/actions/main-view.js
@@ -1,30 +1,6 @@
 import actionTypes from './types';
 import request from '../util/request';
 
-export function getMainView() {
-  return (dispatch) => {
-    return request({
-      dispatch,
-      type: actionTypes.getMainView,
-      query: `
-        query domainQuery {
-          domain
-          {
-          	id
-            name
-            expiring_date
-            registered_date
-            name_server
-            private_whois
-          }
-        }
-      `,
-      variables: {
-      },
-    });
-  };
-}
-
 export function updateWhoisPrivacy(id, privacy) {
   return (dispatch) => {
     return request({
--- shared/containers/domain-detail.jsx
+++ shared/containers/domain-detail.jsx
@@ -2,14 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import * as domainDetailActions from '../../client/actions/domain-detail';
 import DomainDetailView from '../components/domain-detail/domain-detail';
 
 @connect(
   (state) => ({
     domainDetail: state.domainDetail.data[0],
   }),
-  (dispatch) => bindActionCreators(domainDetailActions, dispatch)
+  (dispatch) => bindActionCreators({}, dispatch)
 )
 export default class DomainDetailContainer extends Component {
 
